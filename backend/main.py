b5946e03ed68d6faa68767b142eedb6fe8e8833f
sugasuzu <y_suzuki@sugasuzu.com>
2024-09-28 21:06:32 +0900

    モデルの定義

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,13 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+# Todoアイテムのモデル定義
+class TodoItem(BaseModel):
+    id: int
+    title: str
+    description: str
+    completed: bool = False  # デフォルトはFalse（未完了）
+
 
 app = FastAPI()
 
