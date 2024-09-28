944bc6dcc6ba3bc004c5368b23f29bba27a00b3b
sugasuzu <y_suzuki@sugasuzu.com>
2024-09-28 21:10:05 +0900

    基本的なAPIエンドポイントの作成

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,19 @@ class TodoItem(BaseModel):
 
 app = FastAPI()
 
+todos = []  # Todoアイテムを格納するリスト
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/todos")
+def get_todos():
+    return todos
+
+@app.post("/todos")
+def create_todo(todo: TodoItem):
+    todos.append(todo)
+    return todo
+
+@app.delete("/todos/{todo_id}")
+def delete_todo(todo_id: int):
+    global todos
+    todos = [todo for todo in todos if todo.id != todo_id]
+    return {"message": "Todo deleted"}
